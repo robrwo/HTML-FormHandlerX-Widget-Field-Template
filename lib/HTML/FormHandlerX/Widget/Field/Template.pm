9be5f9626208b5f678d3a082ed00b681d20d41e9
Robert Rothenberg <rrwo@cpan.org>
2017-10-31 16:02:00 +0000

    Fix reference to field

--- lib/HTML/FormHandlerX/Widget/Field/Template.pm
+++ lib/HTML/FormHandlerX/Widget/Field/Template.pm
@@ -26,21 +26,20 @@ sub render {
       . "'. Field may be inactive."
       unless $result;
 
-    my $field = $result->field_def;
-    my $form  = $field->form;
+    my $form  = $self->form;
 
-    my %args = ( field => $field, maybe c => $form->ctx );
+    my %args = ( field => $self, maybe c => $form->ctx );
 
     if ( my $method = $form->can('template_args') ) {
-        $form->$method( $field, \%args );
+        $form->$method( $self, \%args );
     }
 
-    if ( my $method = $field->can('template_args') ) {
-        $field->$method( \%args );
+    if ( my $method = $self->can('template_args') ) {
+        $self->$method( \%args );
     }
 
-    if ( my $method = $form->can( 'template_args_' . $form->name ) ) {
-        $form->$method( $field, \%args );
+    if ( my $method = $form->can( 'template_args_' . $self->name ) ) {
+        $form->$method( $self, \%args );
     }
 
     return $self->template_renderer->( \%args );
