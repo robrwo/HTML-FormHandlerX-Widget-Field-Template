34a5d046eafdbaf65a776909cd2ceb9ef31e3db1
Robert Rothenberg <rrwo@cpan.org>
2017-10-31 11:05:04 +0000

    Change to use template_renderer method
    The form needs only a template_renderer method that knows how to
    render the arguments.
    
    This allows us to not be reliant on any specific templating class.

--- lib/HTML/FormHandlerX/Widget/Field/Template.pm
+++ lib/HTML/FormHandlerX/Widget/Field/Template.pm
@@ -2,50 +2,21 @@ package HTML::FormHandlerX::Widget::Field::Template;
 
 use Moose::Role;
 
-use MooseX::MungeHas;
 use PerlX::Maybe;
-use Ref::Util qw/ is_ref is_blessed_ref is_coderef /;
 use Template;
 use Types::Standard -types;
 
 use namespace::autoclean;
 
-has template_path => ( is => 'ro', );
-
-has template_path => (
-    is  => 'lazy',
-    isa => Str | CodeRef,
+has template_renderer => (
+    is      => 'ro',
+    isa     => CodeRef,
+    lazy    => 1,
+    default => sub {
+        my ($self) = @_;
+        $self->form->template_renderer($self);
+    },
 );
-
-sub _build_template_path {
-    my ($self) = @_;
-
-    if ( $self->form->can('template_path') ) {
-        return sub { $self->form->template_path($self) };
-    }
-    return;
-}
-
-has template => (
-    is  => 'lazy',
-    isa => InstanceOf ['Template'],
-
-    # coerce => sub {
-    #     my ($value) = @_;
-
-    #     return $value if is_blessed_ref($value) && $value->isa('Template');
-
-    #     return $value->() if is_coderef($value);
-    # },
-);
-
-sub _build_template {
-    my ($self) = @_;
-    if ( $self->form->can('template') ) {
-        return $self->form->template;
-    }
-    return Template->new();
-}
 
 sub render {
     my ( $self, $result ) = @_;
@@ -55,36 +26,24 @@ sub render {
       . "'. Field may be inactive."
       unless $result;
 
-    # FIXME this is a role in a field?
-
     my $field = $result->field_def;
     my $form  = $field->form;
 
-    my %stash = ( field => $field, maybe c => $form->ctx );
+    my %args = ( field => $field, maybe c => $form->ctx );
 
-    if ( my $method = $form->can('template_stash') ) {
-        $form->$method( $field, \%stash );
+    if ( my $method = $form->can('template_args') ) {
+        $form->$method( $field, \%args );
     }
 
-    if ( my $method = $field->can('template_stash') ) {
-        $field->$method( \%stash );
+    if ( my $method = $field->can('template_args') ) {
+        $field->$method( \%args );
     }
 
-    if ( my $method = $form->can( 'template_stash_ ' . $form->name ) ) {
-        $form->$method( $field, \%stash );
+    if ( my $method = $form->can( 'template_args_' . $form->name ) ) {
+        $form->$method( $field, \%args );
     }
 
-    my $tmpl_path = $self->template_path;
-    $tmpl_path = $tmpl_path->() if is_coderef($tmpl_path);
-
-    die "No template is specified for '" . $field->full_name . "'."
-      unless $tmpl_path;
-
-    die "Cannot find template ${tmpl_path}" unless -e $tmpl_path;
-
-    $self->template->process( $tmpl_path, \%stash, \my $output );
-
-    return $output;
+    return $self->template_renderer->( \%args );
 }
 
 1;
--- t/lib/Form.pm
+++ t/lib/Form.pm
@@ -3,13 +3,30 @@ package Form;
 use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler';
 
-has_field a => (
-    widget        => 'Template',
+use Template;
+
+has template => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { Template->new },
 );
 
+has_field a => ( widget => 'Template', );
+
 sub template_path {
-    my ($self, $field) = @_;
+    my ( $self, $field ) = @_;
     return 't/etc/' . $field->name . '.tt';
+}
+
+sub template_renderer {
+    my ( $self, $field ) = @_;
+
+    return sub {
+        my ($args) = @_;
+        $self->template->process( $self->template_path($field),
+            $args, \my $output );
+        return $output;
+    };
 }
 
 1;
